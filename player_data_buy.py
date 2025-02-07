964a2a5a67f338681f6f0a503546b1747562386d
Yan <120256743+xLeg1tQvippi@users.noreply.github.com>
2025-02-07 13:48:31 +0500

    Update player_data_buy.py
    set setting to stock

--- player_data_buy.py
+++ player_data_buy.py
@@ -1,10 +1,1 @@
-player_data = {
-    "вещество": {"total_price": 1365421, "total_quantity": 105, "total_arsenal": 0},
-    "пси_маячок": {"total_price": 970934, "total_quantity": 70, "total_arsenal": 996},
-    "бета": {"total_price": 1148995, "total_quantity": 13, "total_arsenal": 350},
-    "остатки_приборов_шепота": {
-        "total_price": 1129503,
-        "total_quantity": 13,
-        "total_arsenal": 1980,
-    },
-}
+player_data = {}
